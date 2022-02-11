0d0347e01048d9e562f37972f49be83b0c9c2029
svvema <30535884+svvema@users.noreply.github.com>
2022-02-11 11:15:52 +0300

    update

--- ML_inst.py
+++ ML_inst.py
@@ -3,6 +3,7 @@ import numpy as np
 import os, joblib
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+from scipy import signal
 
 import lightgbm as lgb
 from imblearn.over_sampling import SMOTE
@@ -16,10 +17,62 @@ class ml_learn:
         print('reb_df_shape', df.shape)
         self.X_fft_row = self.X_to_fft(df)  # Преобразуем временной ряд в ряд фурье
         self.X_stats_fft_row = self.stat_X(self.X_fft_row, 0, 625)  # Считаем статистики по ряду фурье
-        self.y_class = df.iloc[:, 0]
-        self.y_range = df.iloc[:, 1]
-        self.X_time_row = df.iloc[:, 2:1252]
-        self.X_stats_row = df.iloc[:, 1252:]
+        self.y_class = df.iloc[:, 0]  # Метки класса
+        self.y_range = df.iloc[:, 1]  # Метки дистанции
+        self.X_time_row = df.iloc[:, 300:800]   # Сигнал находиться в этом диапазоне
+        self.X_stats_row = df.iloc[:, 1252:]    # Статистики сигнала
+        self.our_signal = np.array([-0.0056246,
+ 0.010635708022452395,
+ 0.028116350000000012,
+ 0.0453954296995413,
+ 0.0607170999999999,
+ 0.07163818474922215,
+ 0.0770008864221723,
+ 0.0773808,
+ 0.0722873015634411,
+ 0.0614831198082408,
+ 0.044384700000000006,
+ 0.02137332845148135,
+ -0.004796986110708945,
+ -0.03211550000000002,
+ -0.05870369321529817,
+ -0.0827618372945357,
+ -0.1027632576561792,
+ -0.1171836948288788,
+ -0.124215,
+ -0.12325599999999999,
+ -0.1129155,
+ -0.09372516656131545,
+ -0.068445,
+ -0.03845235,
+ -0.007153099999999996,
+ 0.02352299999999999,
+ 0.05277399999999999,
+ 0.078214,
+ 0.09721199999999999,
+ 0.108909,
+ 0.11288385292896957,
+ 0.1094436856159571,
+ 0.09987222549728644,
+ 0.08532700000000001,
+ 0.0653842114369568,
+ 0.042731500000000006,
+ 0.0194925865424128,
+ -0.0023554999999999965,
+ -0.020661066448643947,
+ -0.035411,
+ -0.0468952999999999,
+ -0.054720500000000005,
+ -0.058524999999999994,
+ -0.058018449999999985,
+ -0.053625000000000006,
+ -0.04580228587696292,
+ -0.03580649999999999,
+ -0.02510800000000002,
+ -0.015163999999999983,
+ -0.006877000000000022,
+ 0.00040191719126708714])  # Выделеный сигнал для кореляции
+        self.X_time_row_corr = self.X_time_row.apply(lambda x: signal.correlate(x, self.our_signal, mode='same'))
         self.random_state = 42
         self.models = {}
 
@@ -27,8 +80,9 @@ class ml_learn:
         # self.models.update(self.random_forest('X_stats_row', self.X_stats_row, self.y_class, self.y_range))  # Модели случайного леса по статистикам временного ряда
         self.models.update(self.random_forest('X_fft_row', self.X_fft_row, self.y_class, self.y_range))  # Модели случайного леса по рядам фурье
         # self.models.update(self.random_forest('X_stats_fft_row', self.X_stats_fft_row, self.y_class, self.y_range))  # Модели случайного леса по статистикам по рядам фурье
+        self.models.update(self.random_forest('X_time_row_corr', self.X_time_row_corr, self.y_class, self.y_range))
 
-        self.models.update(self.lgb('X_time_row_gb', self.X_time_row, self.y_class, self.y_range))  # Модели гб по полному временному ряду
+        # self.models.update(self.lgb('X_time_row_gb', self.X_time_row, self.y_class, self.y_range))  # Модели гб по полному временному ряду
         # self.models.update(self.lgb('X_stats_row_gb', self.X_stats_row, self.y_class, self.y_range))  # Модели гб по статистикам временного ряда
         # self.models.update(self.lgb('X_fft_row_gb', self.X_fft_row, self.y_class, self.y_range))  # Модели гб по рядам фурье
         # self.models.update(self.lgb('X_stats_fft_row_gb', self.X_stats_fft_row, self.y_class, self.y_range))  # Модели гб по статистикам по рядам фурье
@@ -96,10 +150,10 @@ class ml_learn:
 
     def X_to_fft(self, df):
         c = []
-        for i in df.iloc[:, 2:1252].itertuples(index=False):
+        for i in df.iloc[:, 300:800].itertuples(index=False):
             # print(i[1:])
             X_fft = np.fft.fft(i)
-            c.append(np.abs(X_fft)[: (1250 // 2)])
+            c.append(np.abs(X_fft)[: (500 // 2)])
         return pd.DataFrame(c)
 
     def stat_X(self, df, start_column, stop_column):
@@ -113,9 +167,9 @@ class ml_learn:
         return stat
 
 
-# path = r'C:\Users\Админ\PycharmProjects\Work_task'
+# path = r'C:\Users\admin\PycharmProjects\qt-proj\settings\signals'
 # df = pd.read_csv(path + "\data.csv")
-# ml = ml_learn(r"C:\Users\Админ\PycharmProjects\Work_task")
+# ml = ml_learn(r"C:\Users\admin\PycharmProjects\qt-proj\settings")
 # print(ml.models)
 # ml.save_models()
 
@@ -124,6 +178,57 @@ class ml_predict:
     def __init__(self, paths):
         self.path = paths
         self.models = self.load_models()
+        self.our_signal = np.array([-0.0056246,
+                                    0.010635708022452395,
+                                    0.028116350000000012,
+                                    0.0453954296995413,
+                                    0.0607170999999999,
+                                    0.07163818474922215,
+                                    0.0770008864221723,
+                                    0.0773808,
+                                    0.0722873015634411,
+                                    0.0614831198082408,
+                                    0.044384700000000006,
+                                    0.02137332845148135,
+                                    -0.004796986110708945,
+                                    -0.03211550000000002,
+                                    -0.05870369321529817,
+                                    -0.0827618372945357,
+                                    -0.1027632576561792,
+                                    -0.1171836948288788,
+                                    -0.124215,
+                                    -0.12325599999999999,
+                                    -0.1129155,
+                                    -0.09372516656131545,
+                                    -0.068445,
+                                    -0.03845235,
+                                    -0.007153099999999996,
+                                    0.02352299999999999,
+                                    0.05277399999999999,
+                                    0.078214,
+                                    0.09721199999999999,
+                                    0.108909,
+                                    0.11288385292896957,
+                                    0.1094436856159571,
+                                    0.09987222549728644,
+                                    0.08532700000000001,
+                                    0.0653842114369568,
+                                    0.042731500000000006,
+                                    0.0194925865424128,
+                                    -0.0023554999999999965,
+                                    -0.020661066448643947,
+                                    -0.035411,
+                                    -0.0468952999999999,
+                                    -0.054720500000000005,
+                                    -0.058524999999999994,
+                                    -0.058018449999999985,
+                                    -0.053625000000000006,
+                                    -0.04580228587696292,
+                                    -0.03580649999999999,
+                                    -0.02510800000000002,
+                                    -0.015163999999999983,
+                                    -0.006877000000000022,
+                                    0.00040191719126708714])
 
     def load_models(self):  # Загружаем модели из конфигурационного файла
         f = open(self.path + '/models/conf.txt', 'r')
@@ -141,10 +246,16 @@ class ml_predict:
         print(models_keys)
         for key in models_keys:
             if key == 'X_time_row':
-                _X = x.reshape(1,-1)
+                _X = signal.correlate(x, self.our_signal).reshape(1, -1)
                 # print('X_time_row',_X)
                 self.add_predict(_X, key, predict_c, predict_r)
                 # print(predict_c)
+            elif key == 'X_time_row_corr':
+                _X = x.reshape(1,-1)
+                # print('X_stats_row',_X)
+                self.add_predict(_X, key, predict_c, predict_r)
+                # print(predict_c)
+
             elif key == 'X_stats_row':
                 _X = self.stat_x(x)
                 # print('X_stats_row',_X)
